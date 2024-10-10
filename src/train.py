ec4150aff4c83f5e2714acf756482e13956ff483
Joshua Inyang <joshuainyang255@gmail.com>
2024-10-11 00:52:48 +0100

    initial commit

---
+++ src/train.py
@@ -0,0 +1,10 @@
+# MODEL TRAINING PIPELINE
+# 1. Data Preprocessing
+# 2. Model Selection
+# 3. Model Training
+# 4. Model Evaluation
+# 5. Model Deployment
+# 6. Model Maintenance
+# 7. Model Monitoring
+# 8. Model Update
+
